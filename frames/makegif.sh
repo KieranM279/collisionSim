8b3bf54f460795c41124619790a64b623a7ffcff
KieranM279 <kieranm279@gmail.com>
2024-11-19 15:38:45 +0000

    begin working on particle particle collision detection

---
+++ frames/makegif.sh
@@ -0,0 +1,5 @@
+convert                                                  \
+  -delay 5                                              \
+   $(for i in $(seq 0 299); do echo ${i}_frame.png; done) \
+  -loop 0                                                \
+   one_ball.gif
