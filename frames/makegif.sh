5b6b866b453fa5d6921988f5eedb66225196689f
KieranM279 <kieranm279@gmail.com>
2024-12-05 14:57:09 +0000

    added a sub-par collision respons response function

--- frames/makegif.sh
+++ frames/makegif.sh
@@ -2,4 +2,4 @@ convert                                                  \
   -delay 5                                              \
    $(for i in $(seq 0 299); do echo ${i}_frame.png; done) \
   -loop 0                                                \
-   first_collision_ball.gif
+   wicked_fast_ball.gif
